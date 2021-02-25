4326a30bcd42b4aeb2c0df6edbcd36e447daed04
thomas-rodrigues <thomasmichels.bnu@gmail.com>
2021-02-25 13:43:59 -0300

    aplicativo de exercicio pronto

---
+++ AULA103/app/src/main/java/com/thomas/aula103/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.thomas.aula103;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        if (getActionBar() != null) {
+            getActionBar().hide();
+        }
+    }
+}
