3ce1128c84e07bcf788b9921d36bf5071c81f616
thomas-rodrigues <thomasmichels.bnu@gmail.com>
2021-03-02 11:15:12 -0300

    Criando projeto do relogio e inserindo elementos da interface

---
+++ AppRelogio/app/src/main/java/com/thomas/apprelogio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.thomas.apprelogio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textHourMinute = findViewById(R.id.text_hour_minute);
+        this.mViewHolder.textSeconds = findViewById(R.id.text_seconds);
+        this.mViewHolder.textBattery = findViewById(R.id.text_battery);
+    }
+
+    private static class ViewHolder {
+        TextView textHourMinute;
+        TextView textSeconds;
+        TextView textBattery;
+    }
+}
