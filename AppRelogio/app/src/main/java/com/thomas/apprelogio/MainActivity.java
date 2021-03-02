0f084316b16da491d03f3d10572536b14d0f2bde
thomas-rodrigues <thomasmichels.bnu@gmail.com>
2021-03-02 11:43:23 -0300

    Adicionando thread para atualizar o horário do relogio

--- AppRelogio/app/src/main/java/com/thomas/apprelogio/MainActivity.java
+++ AppRelogio/app/src/main/java/com/thomas/apprelogio/MainActivity.java
@@ -2,12 +2,33 @@ package com.thomas.apprelogio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable;
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            mViewHolder.textBattery.setText(String.format("%s%%", level));
+        }
+    };
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +38,25 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textHourMinute = findViewById(R.id.text_hour_minute);
         this.mViewHolder.textSeconds = findViewById(R.id.text_seconds);
         this.mViewHolder.textBattery = findViewById(R.id.text_battery);
+
+        this.registerReceiver(this.mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        this.startClock();
+    }
+
+    private void startClock() {
+
+        this.mRunnable = new Runnable() {
+            @Override
+            public void run() {
+
+                long now = SystemClock.elapsedRealtime();
+                long next = 1000 + (1000 - (now % 1000));
+
+                mHandler.postAtTime(mRunnable, next);
+            }
+        };
+        mRunnable.run();
     }
 
     private static class ViewHolder {
