92a7462f7b00fa2352ce2c950f5f7f21afdfe7d8
thomas-rodrigues <thomasmichels.bnu@gmail.com>
2021-03-03 10:51:04 -0300

    app concluido com modo noturno

--- AppRelogio/app/src/main/java/com/thomas/apprelogio/MainActivity.java
+++ AppRelogio/app/src/main/java/com/thomas/apprelogio/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private ViewHolder mViewHolder = new ViewHolder();
     private Handler mHandler = new Handler();
     private Runnable mRunnable;
+    private boolean mTicker = false;
+    private boolean mLandscape = false;
 
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -38,13 +41,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         this.mViewHolder.textHourMinute = findViewById(R.id.text_hour_minute);
         this.mViewHolder.textSeconds = findViewById(R.id.text_seconds);
         this.mViewHolder.textBattery = findViewById(R.id.text_battery);
+        this.mViewHolder.textNight = findViewById(R.id.text_night);
+    }
 
+    @Override
+    protected void onResume() {
+        this.mTicker = true;
         this.registerReceiver(this.mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-
         this.startClock();
+
+        this.mLandscape = (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        this.mTicker = false;
+        this.unregisterReceiver(this.mReceiver);
+        super.onPause();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+
+                            // Esconde nav bar e status bar
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
+        }
     }
 
     private void startClock() {
@@ -55,6 +94,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
 
+                if (!mTicker) {
+                    return;
+                }
                 calendar.setTimeInMillis(System.currentTimeMillis());
                 int hour = calendar.get(Calendar.HOUR_OF_DAY);
                 int minutes = calendar.get(Calendar.MINUTE);
@@ -62,8 +104,15 @@ public class MainActivity extends AppCompatActivity {
 
                 // %d = converter inteiro em string - 02 = casas decimais
                 mViewHolder.textHourMinute.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minutes));
-
                 mViewHolder.textSeconds.setText(String.format(Locale.getDefault(),"%02d", seconds));
+
+                if (mLandscape) {
+                    if (hour >= 18) {
+                        mViewHolder.textNight.setVisibility(View.VISIBLE);
+                    } else {
+                        mViewHolder.textNight.setVisibility(View.GONE);
+                    }
+                }
 
                 long now = SystemClock.elapsedRealtime();
                 long next = now + (1000 - (now % 1000));
@@ -78,5 +127,6 @@ public class MainActivity extends AppCompatActivity {
         TextView textHourMinute;
         TextView textSeconds;
         TextView textBattery;
+        TextView textNight;
     }
 }
